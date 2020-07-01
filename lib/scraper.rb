8a9130ab40f19c85da89ac3d60faec28916c1e83
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-07-01 04:16:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,8 +19,12 @@ class Scraper
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
+  
   def get_courses
     self.get_page.css("h2")
+  end
+  
+  def make_courses
   
 end
 
